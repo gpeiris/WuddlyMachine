bd63ecda75b2fc0fa10ce4a92b91035f5d60c050
gpeiris <56284756+gpeiris@users.noreply.github.com>
2021-06-30 15:42:03 +1000

    Update wuddlymachine.py
    Using a dictionary to store Wuddly egg piles instead of a list

--- wuddlymachine.py
+++ wuddlymachine.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 from IPython.display import Image
 
 # Incubator's Initial State
-WuddlySet = [['Cuddly',23],['Duddly',33],['Fuddly',43]]
+WuddlySet = {'Cuddly':23,'Duddly':33,'Fuddly':43}
 
 def WuddlyMachine(x):
-    trip = 100 #
+    trip = 999 
     while True:
         egg = input("Which egg would you like? (Cuddly/Duddly/Fuddly): ")
         if egg not in ('Cuddly','Duddly','Fuddly'):
@@ -15,29 +15,25 @@ def WuddlyMachine(x):
         else:
             break
     vis = input("Would you like to see the fusion process? (Y/N): ")
-    # Add one egg to the initial state of the Incubator
-    if egg == 'Cuddly':
-        WuddlySet[0][1] += 1
-    elif egg == 'Duddly':
-        WuddlySet[1][1] += 1
-    elif egg == 'Fuddly':
-        WuddlySet[2][1] += 1
-    # Repeat operation until the sum of eggs in the incubator is less than 1
-    while trip > 1:
-        eggTot = 0
-        WuddlySet.sort(key = lambda x: x[1]) # sort egg piles by the number of eggs in each ones
-        WuddlySet[1][1] -= 1 # Remove an egg from the two
-        WuddlySet[2][1] -= 1 #    most plentiful piles
-        WuddlySet[0][1] += 1 # Add an egg to the remaining pile
-        for i in range(3):
-            eggTot += WuddlySet[i][1] # count the number of eggs in the incubator at each step
-        trip = eggTot # if total number of eggs is 1, operation is complete
-        if vis == 'Y':
+    
+    WuddlySet[egg] += 1 # Add one egg to the initial state of the Incubator
+    
+    while trip > 1: # Repeat operation until the sum of eggs in the incubator is less than 1
+        eggTot = 0 
+        low = [key for key in WuddlySet if WuddlySet[key] == min(WuddlySet.values())] # Identify lowest key in dict
+        for wuddly in WuddlySet:
+            if wuddly == low[0]:
+                WuddlySet[wuddly] += 1 # if lowest key, add one egg
+            else:
+                WuddlySet[wuddly] -= 1 # if not the lowest key, add subtract egg
+            eggTot += WuddlySet[wuddly] # count the number of eggs after each fusion
+        trip = eggTot # if total number of eggs is 1, operation is complete, trip will fire
+        if vis == 'Y': # Visualise the egg piles change
             print(WuddlySet)
     return egg
 
 print(WuddlySet)
-egg = WuddlyMachine(0)
+egg = WuddlyMachine(1)
 print('\n ---------------- \n The final incubator state:')
 print(WuddlySet)
 print('\n Congratulations on your new',egg)
